7e3b2afc0f908e2de3d70b122faf5c568b648e13
Samuel Velasquez <jvelasquez1905@gmail.com>
2021-09-29 16:18:04 -0400

    login code and redirect

--- views/index.php
+++ views/index.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    $welcomeMessage = "Welcome to the member's area, " . $_SESSION['username'] . "!";
+} else {
+    header('Location: login.php');
+}
+?>
+
 <!DOCTYPE html>
 <html lang="es">
     <head>
