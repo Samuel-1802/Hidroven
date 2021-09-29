07ed91797344a8494e62cef3f493d0415dabf91b
Samuel Velasquez <jvelasquez1905@gmail.com>
2021-09-29 19:22:18 -0400

    test

--- php/auth.php
+++ php/auth.php
@@ -1,6 +1,7 @@
 <?php
     session_start(); 
 
+    // Iniciar/Cerrar sesión
     if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
         $_SESSION['loggedin'] = false;
         header('Location: login.php');
@@ -10,3 +11,18 @@
     }
     
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>Hidroven</title>
+        
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/style.css">
+    </head>  
+    <body>
+        <!-- Dummy -->
+    </body>
+</html>
