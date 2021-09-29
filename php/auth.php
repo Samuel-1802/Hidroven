5bc011429e5f6dbf73b59fdbd4a1a63c3a1cc647
Samuel Velasquez <jvelasquez1905@gmail.com>
2021-09-29 18:28:19 -0400

    dummy login with auth

---
+++ php/auth.php
@@ -0,0 +1,21 @@
+<?php
+    session_start(); 
+
+    $_SESSION['loggedin'] = true;
+    header('Location: index.php');
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>Hidroven</title>
+        
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>  
+    <body>
+
+    </body>
+</html>
---
+++ php/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>Ingreso</title>
+        
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+
+    <body class="container-fluid">
+        
+        <h3 class="d-flex justify-content-center">Inicio de Sesión</h3>
+        <br />
+        
+        <div class="container d-flex justify-content-center">
+            <!-- Form para el login -->
+            <form action="./auth.php" method="POST">
+                <div class="form-group">
+                    <label for="Username">Usuario</label>
+                    <input type="email" class="form-control" id="Username" aria-describedby="emailHelp" placeholder="Nombre de usuario">
+                </div>
+                <div class="form-group">
+                    <label for="Password">Clave</label>
+                    <input type="password" class="form-control" id="Password" placeholder="Clave">
+                </div>
+                <button type="submit" class="btn btn btn-primary m-2">Entrar</button>
+            </form>
+        </div>
+
+    </body>
+</html>
