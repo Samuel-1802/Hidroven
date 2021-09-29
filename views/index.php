20471e9340a59eef2a93bbc0e8454c1ab4c18b2e
Samuel Velasquez <jvelasquez1905@gmail.com>
2021-09-29 13:44:55 -0400

    basic index.php setup fixed

--- views/index.php
+++ views/index.php
@@ -1,5 +1,18 @@
-<?php 
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>Hidroven</title>
+        
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>  
+    <body>
+    <?php 
 
-echo "Hello world";
+    echo "Hello world";
 
-?>
+    ?>
+    </body>
+</html>
