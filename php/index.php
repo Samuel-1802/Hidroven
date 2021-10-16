6b7de0ecf2c4d21c5a4f06c1e7ef189542fd22cf
Samuel Velasquez <jvelasquez1905@gmail.com>
2021-10-16 18:12:53 -0400

    tweeter embed

--- php/index.php
+++ php/index.php
@@ -12,43 +12,47 @@ include "../assets/header.php";
     }
     ?>
 </div>
-
 <br>
 
-<div>
-    <h3>Home</h3>
+<div class="container d-flex row">
+    <div class="container col col-9">
+        <div>
+            <h3>Home</h3>
+        </div>
+        <br>
+        <div>
+            <h3>
+                Historia
+            </h3>
+            <p>
+                La Casa Matriz del Sector Agua Potable y Saneamiento (Sector APS) fue constituida el 24 de mayo de 1990, una vez liquidado el Instituto Nacional de Obras Sanitarias, INOS. Comienza a funcionar conjuntamente con diez Empresas Hidrológicas Regionales, teniendo como responsabilidad desarrollar políticas y programas en materia de abastecimiento de Agua Potable, Recolección y Tratamiento de Aguas Servidas y Drenajes Urbanos, así como el establecimiento de directrices para la administración, operación, mantenimiento y ampliación de los sistemas atendidos por cada una de sus Filiales; regula y supervisa a las empresas hidrológicas filiales y descentralizadas. Hace cumplir la Ley Orgánica para la prestación de los servicios de agua potable y saneamiento incentiva la participación ciudadana, y desarrolla proyectos planteados por las comunidades y las Mesas Técnicas de Agua.
+            </p>
+        </div>
+        <div>
+            <h3>
+                Misión
+            </h3>
+            <p>
+                La misión de la estructura normativa que regula la administración, en el sentido más amplio, de las aguas, es la de coadyuvar al desarrollo integral y sustentable, a través de la ejecución de proyectos de investigación y acción que modifiquen y mejoren la calidad de vida dando respuesta y desarrollo al artículo 304 de la Constitución Nacional que enmarca a éste recurso natural como un bien de dominio público de la Nación, indispensable y por ende insustituible para la vida y el desarrollo. Es decir, establecer las disposiciones necesarias para garantizar la protección del vital recurso hídrico, su aprovechamiento y recuperación, respetando las fases del ciclo hidrológico y los criterios de ordenación del territorio, bajo una organización institucional capacitada para cumplir y hacer cumplir las mismas.
+            </p>
+        </div>
+        <div>
+            <h3>
+                Visión
+            </h3>
+            <p>
+                La Compañía, tiene como misión ampliar la actuación de HIDROVEN a la ejecución de proyectos en los sectores de agua potable y saneamiento, manteniendo la regulación, rectoría y supervisión de las filiales y descentralizadas.
+            </p>
+        </div>
+    </div>
+
+
+<div class="container col col-3">
+    <a class="twitter-timeline" data-lang="es" data-width="300" data-height="600" href="https://twitter.com/HidrovenOficial?ref_src=twsrc%5Etfw">Tweets by HidrovenOficial</a>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 </div>
 
-<br>
-
-<div>
-    <h3>
-        Historia
-    </h3>
-    <p>
-        La Casa Matriz del Sector Agua Potable y Saneamiento (Sector APS) fue constituida el 24 de mayo de 1990, una vez liquidado el Instituto Nacional de Obras Sanitarias, INOS. Comienza a funcionar conjuntamente con diez Empresas Hidrológicas Regionales, teniendo como responsabilidad desarrollar políticas y programas en materia de abastecimiento de Agua Potable, Recolección y Tratamiento de Aguas Servidas y Drenajes Urbanos, así como el establecimiento de directrices para la administración, operación, mantenimiento y ampliación de los sistemas atendidos por cada una de sus Filiales; regula y supervisa a las empresas hidrológicas filiales y descentralizadas. Hace cumplir la Ley Orgánica para la prestación de los servicios de agua potable y saneamiento incentiva la participación ciudadana, y desarrolla proyectos planteados por las comunidades y las Mesas Técnicas de Agua.
-    </p>
 </div>
-
-<div>
-    <h3>
-        Misión
-    </h3>
-    <p>
-        La misión de la estructura normativa que regula la administración, en el sentido más amplio, de las aguas, es la de coadyuvar al desarrollo integral y sustentable, a través de la ejecución de proyectos de investigación y acción que modifiquen y mejoren la calidad de vida dando respuesta y desarrollo al artículo 304 de la Constitución Nacional que enmarca a éste recurso natural como un bien de dominio público de la Nación, indispensable y por ende insustituible para la vida y el desarrollo. Es decir, establecer las disposiciones necesarias para garantizar la protección del vital recurso hídrico, su aprovechamiento y recuperación, respetando las fases del ciclo hidrológico y los criterios de ordenación del territorio, bajo una organización institucional capacitada para cumplir y hacer cumplir las mismas.
-    </p>
-</div>
-
-<div>
-    <h3>
-        Visión
-    </h3>
-    <p>
-        La Compañía, tiene como misión ampliar la actuación de HIDROVEN a la ejecución de proyectos en los sectores de agua potable y saneamiento, manteniendo la regulación, rectoría y supervisión de las filiales y descentralizadas.
-    </p>
-</div>
-
-
 <?php
 include "../assets/footer.php";
 ?>
